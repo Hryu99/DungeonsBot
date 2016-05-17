b92cf84811a08282876c91c5c13738620865dee2
Yura.K <y.klushin@gmail.com>
2016-05-18 01:58:47 +0300

    Добавлен класс для работы с реквестами. Протестировано на примере.

--- DungeonsBot/Program.cs
+++ DungeonsBot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 using System.Net;
 using System.IO;
 using System.Collections.Generic;
@@ -12,7 +13,35 @@ namespace DungeonsBot
     {
         static void Main()
         {
-            string url = "game-r06ru.rjgplay.com";
+            string data = "";
+            string uid = "fb:924660480936953";
+            string auth = "ab3ea4154fc2a9313aa3e16f98d244ad";
+
+            //получаем sid
+            data = SendRequest("/command/get_game_info", string.Format(@"<get_game_info uid=""{0}"" auth_key=""{1}""/>", uid, auth));
+            data.IndexOf("sid=\"");
+
+            //разобраться как парсить ХМЛ, вынимать из него необходимые данные и тп
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(data);
+            
+           
+
+
+
+            Console.WriteLine("2");
+             
+            Console.ReadKey();
+        }
+
+        static string SendRequest(string command, string requestBody)
+        {
+            string host = "https://game-r06ww.rjgplay.com";
+            string url = host + command;
+
+            MyWebRequest test = new MyWebRequest(url, "POST", requestBody);
+            return test.GetResponse();
         }
     }
 }
